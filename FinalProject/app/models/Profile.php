ad9405b07c462b4921a34c5cf3c145cfbbe629cc
wornsoulbit <alexandervasil840@gmail.com>
2021-04-23 21:15:29 -0400

    Created profile model class

---
+++ FinalProject/app/models/Profile.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\models;
+
+class Profile extends \App\core\Model {
+
+    public $first_name;
+    public $last_name;
+    public $user_id;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function find($profile_id) {
+        $stmt = self::$connection->prepare("SELECT * FROM profile WHERE profile_id = :profile_id");
+        $stmt->execute(['profile_id' => $profile_id]);
+        $stmt->setFetchMode(\PDO::FETCH_GROUP | \PDO::FETCH_CLASS, "App\\models\\Profile");
+        return $stmt->fetch();
+    }
+
+    public function findUserId($user_id) {
+        $stmt = self::$connection->prepare("SELECT * FROM user WHERE user_id = :user_id");
+        $stmt->execute(['user_id' => $user_id]);
+        $stmt->setFetchMode(\PDO::FETCH_GROUP | \PDO::FETCH_CLASS, "App\\models\\Profile");
+        return $stmt->fetch();
+    }
+
+    public function insert() {
+        $stmt = self::$connection->prepare("INSERT INTO profile(user_id, first_name, last_name) VALUES (:user_id, :first_name, :last_name)");
+        $stmt->execute(['user_id' => $this->user_id, 'first_name' => $this->first_name, 'last_name' => $this->last_name]);
+    }
+
+    public function update() {
+        $stmt = self::$connection->prepare("UPDATE profile SET first_name=:first_name, last_name=:last_name WHERE profile_id=:profile_id");
+        $stmt->execute(['first_name' => $this->first_name, 'last_name' => $this->last_name, 'profile_id' => $this->profile_id]);
+    }
+
+}
+
+?>
