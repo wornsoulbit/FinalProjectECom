8f6cb29ed92b259208eceb8c50af9b1b8af3536f
wornsoulbit <alexandervasil840@gmail.com>
2021-04-23 21:16:49 -0400

    created the views for the profile, both a profilePage view and a createProfile view

---
+++ FinalProject/app/views/Profile/createProfile.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Create Profile</title>
+    </head>
+
+    <body>
+        <h3>Create your Profile</h3>
+
+        <form method="post" action="">
+            <label>First Name: <input type="text" name="first_name"></label><br />
+            <label>Last Name: <input type="text" name="last_name"></label><br />
+
+            <input type="submit" name="action" value="Create Profile" />
+        </form>
+    </body>
+</html>
