47ea51fcc754786b3c9ca7004c14d29ca841f45d
wornsoulbit <alexandervasil840@gmail.com>
2021-04-23 21:53:16 -0400

    added Page model

---
+++ FinalProject/app/models/Page.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\models;
+
+class Page extends \App\core\Model {
+
+    public $profile_id;
+    public $page_title;
+    public $page_text;
+
+    public function find($page_id) {
+        $stmt = self::$connection->prepare("SELECT * FROM page WHERE page_id = :page_id");
+        $stmt->execute(['page_id' => $page_id]);
+        $stmt->setFetchMode(\PDO::FETCH_GROUP | \PDO::FETCH_CLASS, "App\\models\\Profile");
+        return $stmt->fetch();
+    }
+
+    public function insert() {
+        $stmt = self::$connection->prepare("INSERT INTO page(profile_id, page_title, page_text) VALUES (:profile_id, :page_title, :page_text)");
+        $stmt->execute(['profile_id' => $this->profile_id, 'page_title' => $this->page_title, 'page_text' => $this->page_text]);
+    }
+
+    public function update() {
+        $stmt = self::$connection->prepare("UPDATE page SET profile_id=:profile_id, page_title=:page_title WHERE page_text=:page_text");
+        $stmt->execute(['profile_id' => $this->profile_id, 'page_title' => $this->page_title, 'profile_id' => $this->page_text]);
+    }
+
+}
+
+?>
