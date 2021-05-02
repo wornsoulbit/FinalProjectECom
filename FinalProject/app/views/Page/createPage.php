d123649775a1ce61e114630b9efa82b764238aab
wornsoulbit <alexandervasil840@gmail.com>
2021-05-02 03:16:33 -0400

    Added a API text editor to the edit and create pages views

--- FinalProject/app/views/Page/createPage.php
+++ FinalProject/app/views/Page/createPage.php
@@ -1,18 +1,24 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>Create Page</title>
+        <script src="https://cdn.tiny.cloud/1/8qhzpgykznjfoihm9y692wkhbhzhpv1y1ddimfioy7txo9oi/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+        <script src="<?=BASE?>/JS/tinyMce.js"></script>
+        <link rel="stylesheet" href="<?=BASE?>/CSS/PageCss/createPage.css">
     </head>
 
     <body>
-        <h3>Create your Page</h3>
+        <div id="container">
+            <h3>Create your Page</h3>
 
-        <form method="post" action="">
-            <label>Page title: <input type="text" name="page_title"> </label> <br/>
-            <label>Page text: </label> <br />
-            <textarea name="page_text" rows="10" cols="50"></textarea> <br/>
+            <form method="post" action="">
+                <label>Page title: <input type="text" name="page_title"> </label> <br/>
+                <label>Page text: </label> <br />
+                <textarea id="page_text" name="page_text" rows="30"> </textarea> <br/>
 
-            <input type="submit" name="action" value="Create Page" />
-        </form>
-        <a href="<?= BASE ?>/Page/index/<?= $_SESSION['profile_id']?>">Return to pages list</a> <br />
+                <input type="submit" name="action" value="Create Page" />
+            </form>
+            <a href="<?= BASE ?>/Page/index/<?= $_SESSION['profile_id']?>">Return to pages list</a> <br />
+        </div>
     </body>
 </html>
--- FinalProject/app/views/Page/editPage.php
+++ FinalProject/app/views/Page/editPage.php
@@ -1,7 +1,10 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>Edit Page</title>
         <link rel="stylesheet" href="<?=BASE?>/CSS/PageCss/editPage.css">
+        <script src="https://cdn.tiny.cloud/1/8qhzpgykznjfoihm9y692wkhbhzhpv1y1ddimfioy7txo9oi/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+        <script src="<?=BASE?>/JS/tinyMce.js"></script>
     </head>
 
     <body>
@@ -12,7 +15,7 @@
                 <div id="pageContents">
                     <label>Page title: <input type="text" name="page_title" value="<?= $data['page']->page_title ?>"></label><br />
                     <label>Page text: </label><br />
-                    <textarea name="page_text" rows="10" cols="50"><?= $data['page']->page_text ?></textarea><br />
+                    <textarea id="page_text" name="page_text" rows="30"><?= $data['page']->page_text ?></textarea><br />
                     <input type="submit" name="action" value="Submit changes" />
                 </div>
             </form>
