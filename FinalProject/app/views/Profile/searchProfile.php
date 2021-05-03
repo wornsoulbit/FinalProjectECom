7fdc51c0cfe16df1f96181dcd22a87a1561453ff
wornsoulbit <alexandervasil840@gmail.com>
2021-05-03 13:28:19 -0400

    added css styling to searchPage

--- FinalProject/app/views/Profile/searchProfile.php
+++ FinalProject/app/views/Profile/searchProfile.php
@@ -1,21 +1,25 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Search a user</title>
+	<link rel="stylesheet" href="<?=BASE?>/CSS/ProfileCss/searchPage.css">
 </head>
 <body>
-	<form method="post" action="">
-		<label>Enter the user: <input type="text" name="searchProfile" /></label><br />
-		<input type="submit" name="action" value="Search profile" />
+	<div id="container">
+		<h2>Search a Profile!</h2>
+		<form method="post" action="" id="form">
+			<label id="searchField">Username: <input type="text" name="searchProfile" /></label><br />
+			<input type="submit" name="action" value="Search profile" id="searchButton"/>
 
-	</form>
+		</form>
 
-	<?php
-		if ($data['profile']->profile_id != null) {
-			echo "<a href=\"" . BASE . "/Profile/index/" . $data['profile']->profile_id . "\">cancel</a>";
-		} else {
-			echo "<a href=\"" . BASE . "/Default/index/\">cancel</a>";
-		}
-	?>
-	
+		<?php
+			if ($data['profile']->profile_id != null) {
+				echo "<a href=\"" . BASE . "/Profile/index/" . $data['profile']->profile_id . "\">cancel</a>";
+			} else {
+				echo "<a href=\"" . BASE . "/Default/index/\">cancel</a>";
+			}
+		?>
+	</div>
 </body>
 </html>
