f798391c424482a1e3367e2875ff80816a6f5748
Jamie <lowjamie8@gmail.com>
2021-05-10 20:12:35 -0400

    1 min timeout update

--- FinalProject/app/core/helpers.php
+++ FinalProject/app/core/helpers.php
@@ -15,7 +15,7 @@ function ConvertDateTime(){
 
 function timeoutDateTime(){
 	$date = new DateTime(null, new DateTimezone("UTC"));
-	$date->add(new DateInterval('PT0H2M0S')); //we add 2min, user is timed out for 2 min
+	$date->add(new DateInterval('PT0H1M0S')); //we add 1min, user is timed out for 1 min
 	$tz = new DateTimeZone(date_default_timezone_get());
 	$date->setTimeZone($tz);
 	return $date->format('Y-m-d H:i:sP e');
